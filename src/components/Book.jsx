1907d3fa62dab1564e906ac192cb511230debc1b
Belov Nikita <nintedo.32@gmail.com>
2024-12-19 18:17:49 +0300

    .

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,7 +3,7 @@ export default function Book (name, author, status){
     return(
     <>
     <h1 className="">Название:</h1>
-    <h2 className="shadow-inner shadow-current px-2 mt-3 font-medium h-12 max-h-12 ">{name}</h2>
+    <h2 className="shadow-inner shadow-current px-2 mt-3 font-medium h-12 max-h-12 text-justify-middle content-center text-balance" >{name}</h2>
     <h1 className="mt-3">Автор:</h1>
     <h2 className="shadow-inner shadow-current px-2 mt-3 font-medium">{author}</h2>
     <h1 className="mt-3">Статус:</h1>
